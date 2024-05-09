4acb023e493aade9d94895ca815da6fdee603c73
Jesse Abrahams <jesse.abrahams@gmail.com>
2024-05-09 08:54:39 -0400

    Add wasm-tools to build script to perform optimizations

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --version latest -InstallDir ./dotnet
 ./dotnet/dotnet --version
+./dotnet workload install wasm-tools
 ./dotnet/dotnet publish -c Release -o output
