5bb07e50a48c57ff40ec39f767eccf733a7a0ad3
Jesse Abrahams <jesse.abrahams@gmail.com>
2024-05-09 08:57:38 -0400

    Fix dotnet syntax, add project name to publish cmd

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --version latest -InstallDir ./dotnet
 ./dotnet/dotnet --version
-./dotnet workload install wasm-tools
-./dotnet/dotnet publish -c Release -o output
+./dotnet/dotnet workload install wasm-tools
+./dotnet/dotnet publish BlazorWasmTemplate.csproj -c Release -o output
